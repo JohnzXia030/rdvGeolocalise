ec7bf00875b19c7d34417963622eb30eefe112df
51924 <johnzx030@gmail.com>
2020-12-28 00:17:32 +0100

    add contact layout

--- app/src/main/java/com/example/rdvgeolocalise/MainActivity.java
+++ app/src/main/java/com/example/rdvgeolocalise/MainActivity.java
@@ -1,16 +1,10 @@
 package com.example.rdvgeolocalise;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,32 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Context context = getApplicationContext();
-        PermissionUtils permissionUtils = new PermissionUtils();
-        permissionUtils.request_permissions(context, this);
-    }
+    public void contact(View view){
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 1001:
-                if (grantResults.length > 0) {
-                    // 因为是多个权限，所以需要一个循环获取每个权限的获取情况
-                    for (int i = 0; i < grantResults.length; i++) {
-                        // PERMISSION_DENIED 这个值代表是没有授权，我们可以把被拒绝授权的权限显示出来
-                        if (grantResults[i] == PackageManager.PERMISSION_DENIED){
-                            Toast.makeText(MainActivity.this, permissions[i] + "权限被拒绝了", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-                break;
-        }
+
     }
 }
