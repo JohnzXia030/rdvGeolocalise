e300c61babd726a6680e2131898eaef1a643eb6d
51924 <johnzx030@gmail.com>
2020-12-30 03:17:33 +0100

    添加信息发送

--- app/src/main/java/com/example/rdvgeolocalise/MainActivity.java
+++ app/src/main/java/com/example/rdvgeolocalise/MainActivity.java
@@ -24,12 +24,14 @@ import android.provider.ContactsContract.CommonDataKinds.StructuredName;
 import android.provider.ContactsContract.CommonDataKinds.StructuredPostal;
 import android.provider.ContactsContract.CommonDataKinds.Website;
 import android.provider.ContactsContract.Data;
+import android.telephony.PhoneNumberUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -40,11 +42,14 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CONTACT = 1;
     private JSONObject contactData;
     private JSONObject jsonObject;
+    public static final String NUM_REGEX = "^[+]?[0-9]{10,13}$";
     private static final List<String> totalNumArrayToSend = new ArrayList<String>();
     private static final List<String> contactNumArray = new ArrayList<String>();
     private static final List<String> enteredNumArray = new ArrayList<String>();
@@ -54,63 +59,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        /**
-         * Load contact info
-         */
         setContentView(R.layout.activity_main);
-        ContactUtil contactUtil = new ContactUtil(MainActivity.this);
-        MultiSpinner multiSpinner = (MultiSpinner) findViewById(R.id.multi_spinner);
-        List<String> items = new ArrayList<String>();
-        String contactInfo;
-        try {
-            contactInfo = contactUtil.getContactInfo();
-            JSONObject jsonObject = new JSONObject(contactInfo);
-            Iterator<String> keys = jsonObject.keys();
-            while(keys.hasNext()) {
-                String key = keys.next();
-                JSONObject contact = (JSONObject) jsonObject.get(key);
-                items.add(contact.getString("lastname") + " : " + contact.getString("mobile"));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        multiSpinner.setItems(items, "Select contact", selected -> {
-            contactNumArray.clear();
-            for (int i = 0; i < items.size(); i++) {
-                if (selected[i]) {
-                   // Toast.makeText(MainActivity.this, items.get(i), Toast.LENGTH_SHORT).show();
-                    String num = items.get(i).substring(items.get(i).lastIndexOf(":") + 1);
-                    //Toast.makeText(MainActivity. this, num  , Toast.LENGTH_SHORT).show();
-                    contactNumArray.add(num);
-                }
-            }
-            for (int i = 0; i < contactNumArray.size(); i++) {
-                Toast.makeText(MainActivity.this, contactNumArray.get(i), Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        /**
-         * Add edittext listener
-         */
-        EditText numeroView = (EditText) findViewById(R.id.num);
-        numeroView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-                toast("Veuillez saisir le(s) numéros sous forms de numéro en'NUM1; NUM2; ...'");
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
     }
 
     /**
@@ -139,6 +88,65 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         PermissionUtils permissionUtils = new PermissionUtils();
         permissionUtils.request_permissions(context, this);
+
+
+        /**
+         * Load contact info
+         */
+
+        ContactUtil contactUtil = new ContactUtil(MainActivity.this);
+        MultiSpinner multiSpinner = (MultiSpinner) findViewById(R.id.multi_spinner);
+        List<String> items = new ArrayList<String>();
+        String contactInfo;
+        try {
+            contactInfo = contactUtil.getContactInfo();
+            JSONObject jsonObject = new JSONObject(contactInfo);
+            Iterator<String> keys = jsonObject.keys();
+            while(keys.hasNext()) {
+                String key = keys.next();
+                JSONObject contact = (JSONObject) jsonObject.get(key);
+                items.add(contact.getString("lastname") + " : " + contact.getString("mobile"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        multiSpinner.setItems(items, "Select contact", selected -> {
+            contactNumArray.clear();
+            for (int i = 0; i < items.size(); i++) {
+                if (selected[i]) {
+                    // Toast.makeText(MainActivity.this, items.get(i), Toast.LENGTH_SHORT).show();
+                    String num = items.get(i).substring(items.get(i).lastIndexOf(":") + 1);
+                    //Toast.makeText(MainActivity. this, num  , Toast.LENGTH_SHORT).show();
+                    contactNumArray.add(num);
+                }
+            }
+            for (int i = 0; i < contactNumArray.size(); i++) {
+                Toast.makeText(MainActivity.this, contactNumArray.get(i), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        /**
+         * Add edittext listener
+         */
+        EditText numeroView = findViewById(R.id.numEntered);
+        numeroView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                //toast("Veuillez saisir le(s) numéros sous forms de numéro en'NUM1; NUM2; ...'");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
@@ -165,7 +173,24 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
-    private void toast(String s){
+    public void sendSms(View view){
+        EditText numberView = (EditText) findViewById(R.id.numEntered);
+        String numbers = numberView.getText().toString();
+        String[] arrNumber = numbers.split(";");
+        String formatedNum;
+        for (String num: arrNumber){
+            /*boolean matches =  Pattern.matches(NUM_REGEX, num);
+            toast(num + ">>>>>");
+            if (matches){
+                //enteredNumArray.add(num);
+                toast(num);
+            }*/
+            formatedNum = PhoneNumberUtils.formatNumber(num);
+            toast(formatedNum);
+        }
+       // toast(formatedNum);
+    }
+    public void toast(String s){
         Toast.makeText(getApplication(),s,Toast.LENGTH_SHORT).show();
     }
 }
